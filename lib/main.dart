71e7b1db3de921e87473feabef5bbddc56db834f
Nazeera Alkhouri <NazeeraAlkhoury21@gmail.com>
2024-12-05 11:49:39 +0200

    Integrate flutter_dotenv package: configure .env file, update pubspec.yaml and main.dart, and add .env to .gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,15 @@ import 'package:checkout/core/utils/app_strings.dart';
 import 'package:checkout/core/utils/app_theme.dart';
 import 'package:checkout/features/presentaion/views/my_cart_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: "assets/.env");
+
+  Stripe.publishableKey = dotenv.env['STRIPE_PUBLISHABLE_KEY']!;
+
   runApp(const MyApp());
 }
 
